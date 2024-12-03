698819819e17cd38b8149aa3148fd7c3666b0146
Harsh Kumar <harshsingh1393@gmail.com>
2024-12-03 12:08:28 +0530

    ceated the user controller and user routes

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,19 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require("./db/db.js")
+const userRoutes = require("./routes/user.route.js")
 
-
+connectToDb()
 app.use(cors())
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use("/users",userRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+module.exports = app
---
+++ Backend/routes/user.route.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const router = express.Router()
+const { body } = require("express-validator")
+const userController = require("../controllers/user.controller.js")
+
+router.post('/register',[
+    //here we are validating the data with express-validator
+    body('email').isEmail().withMessage("Inavalid Email"),
+    body("fullname.firstname").isLength({min:3}).withMessage("First name must be at least 3 characters long"),
+    body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long")
+],
+    userController.registerUser
+)
+
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+
+
+module.exports = router
