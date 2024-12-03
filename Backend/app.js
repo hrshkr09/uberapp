b4a75a1f400d38bddcf84fbdc9e6a89197eb8fd1
Harsh Kumar <harshsingh1393@gmail.com>
2024-12-03 16:54:03 +0530

    created the captain part

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser")
 const app = express();
 const connectToDb = require("./db/db.js")
 const userRoutes = require("./routes/user.route.js")
+const captainRoutes = require('./routes/captain.route');
 
 connectToDb()
 app.use(cors())
@@ -13,7 +14,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-app.use("/users",userRoutes)
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
