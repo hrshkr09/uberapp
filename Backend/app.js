96a99338df5b53e43ae7c87699dd70c76bcb0705
Harsh Kumar <harshsingh1393@gmail.com>
2024-12-03 12:50:43 +0530

    completed user part

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser")
 const app = express();
 const connectToDb = require("./db/db.js")
 const userRoutes = require("./routes/user.route.js")
@@ -11,7 +12,7 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
+app.use(cookieParser())
 app.use("/users",userRoutes)
 
 app.get('/', (req, res) => {
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { body } = require("express-validator")
 const userController = require("../controllers/user.controller.js")
-
+const authMiddleware = require("../middlewares/auth.middleware.js")
 router.post('/register',[
     //here we are validating the data with express-validator
     body('email').isEmail().withMessage("Inavalid Email"),
@@ -20,5 +20,7 @@ router.post('/login', [
     userController.loginUser
 )
 
+router.get("/profile", authMiddleware.authUser, userController.getUserProfile)
+router.get("/logout", authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router
