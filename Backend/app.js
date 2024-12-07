9bdc33e1de47360414238a617fc07ebbba6a48c0
Harsh Kumar <harshsingh1393@gmail.com>
2024-12-07 17:35:44 +0530

    completed the map controllers routes services

--- Backend/app.js
+++ Backend/app.js
@@ -7,18 +7,23 @@ const app = express();
 const connectToDb = require("./db/db.js")
 const userRoutes = require("./routes/user.route.js")
 const captainRoutes = require('./routes/captain.route');
+const mapsRoutes = require('./routes/maps.route');
+
+
 
 connectToDb()
 app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
 
 module.exports = app
